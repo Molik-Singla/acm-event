cac3da7c2c29e0856c48c867f4dbb1ce2d5e4828
Molik Singla <moliksingla02@gmail.com>
2024-07-16 13:15:42 +0530

    animate

--- src/components/SingleCountry.jsx
+++ src/components/SingleCountry.jsx
@@ -2,9 +2,13 @@ import CanadaImg from "./../assets/Flags/china.png";
 
 const SingleCountry = ({ name, imgSrc }) => {
 	return (
-		<div className="bg-green-900 p-4 rounded-2xl w-full">
+		<div className="bg-green-900 p-4 rounded-2xl w-full hover:scale-110 duration-300 transition-all hover:rotate-1 group">
 			<div className="w-full flex justify-center relative">
-				<img className="w-32 rounded-full h-32 absolute -top-16" src={imgSrc} alt="img not found" />
+				<img
+					className="w-32 rounded-full h-32 absolute -top-16 group-hover:rotate-[360deg] transition-all duration-[1.5s]"
+					src={imgSrc}
+					alt="img not found"
+				/>
 			</div>
 			<div className="flex flex-col items-center mt-20">
 				<h3 className="text-xl font-semibold">{name}</h3>
